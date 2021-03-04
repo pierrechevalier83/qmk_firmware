f8714e684610837d8165809d2030057709011933
Pierre Chevalier <pierrechevalier83@gmail.com>
2021-03-04 15:52:49 +0000

    Try and use PB10 for I2C2_SCL and PB11 for I2C2_SDA
    Never got any connection with the MCP23017 on the right hand...

--- keyboards/ferris/0_2/config.h
+++ keyboards/ferris/0_2/config.h
@@ -65,3 +65,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 5
 
+/* i2c settings */
+
+#define I2C_DRIVER I2CD2
+#define I2C1_SCL 10
+#define I2C1_SDA 11
